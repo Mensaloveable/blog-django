658b9e15535cf91002262256dadd4d5421b1fe92
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-09-03 10:46:34 +0100

    create post DB

--- blog/views.py
+++ blog/views.py
@@ -1,34 +1,13 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Mensa',
-        'title': 'Love',
-        'content': 'How to be loveabe',
-        'date': 'August 27 2023'
-    },
-    {
-        'author': 'Loveable',
-        'title': 'Mensa',
-        'content': 'How to be a king',
-        'date': 'August 18 2023'
-    },
-    {
-        'author': 'Great',
-        'title': 'Mensa',
-        'content': 'How to be a king'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
     return render(request, 'blog/about.html')
-
-# /Users/decagon/Desktop/dev/Django/django_project/blog/template/blog/home.html
