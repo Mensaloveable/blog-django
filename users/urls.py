0521459c9ae7ade8cd7f2cff90fa7f5457cbf043
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-09-03 22:41:38 +0100

    Fix: refactor nav bar

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(), name="login"),
-    path('logout/', LogoutView.as_view(), name="logout")
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name="logout")
 ]
