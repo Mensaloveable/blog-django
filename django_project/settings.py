cfb194ba62ca8ec81cdd3a73c8b028db05240fee
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-09-03 13:19:43 +0100

    Fix: correction to UsersConfig definition

--- django_project/settings.py
+++ django_project/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'users.apps.AppConfig',
+    'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
