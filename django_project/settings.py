664c3d457f9b79e3c9c64b02223679c138d5bfd4
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-09-03 20:06:14 +0100

    Feat: add crispy forms to project

--- django_project/settings.py
+++ django_project/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'blog.apps.BlogConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
